55a7102f6217a69770ff436f3dd76ce3af65eefa
Kaytee07 <kofi.taylor07@gmail.com>
2021-09-24 11:27:49 +0000

    creating a submenu with jquery

--- app/app.js
+++ app/app.js
@@ -2,14 +2,22 @@ $(document).ready(function(){
 
 let el = document.getElementById("text");
 
-console.log("EL in vanilaa js", el);
-console.log("EL in jquery", $(el)[0]);
 
 $("#text").html("This is now my text");
 $(".my-input").val("New Input Val");
 
 $("#go-button").on("mouseleave",function(){
     alert("yo gee")
+})
+
+$('[data-trigger="dropdown"]').on('mouseenter', function(){
+  let submenu = $(this).parent().find('.profile-submenu')
+  submenu.fadeIn(300)
+
+  $('.profile-menu').on('mouseleave', function(){
+      submenu.fadeOut(300);
+  })
+    
 })
 
 });
