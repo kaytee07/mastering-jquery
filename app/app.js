cde39e07f1c5a99c3d34d83c571daa36c80c3cca
Kaytee07 <kofi.taylor07@gmail.com>
2021-09-24 10:14:47 +0000

    event handling with jquery

--- app/app.js
+++ app/app.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 
+let el = document.getElementById("text");
+
+console.log("EL in vanilaa js", el);
+console.log("EL in jquery", $(el)[0]);
+
 $("#text").html("This is now my text");
 $(".my-input").val("New Input Val");
+
+$("#go-button").on("mouseleave",function(){
+    alert("yo gee")
+})
 
 });
